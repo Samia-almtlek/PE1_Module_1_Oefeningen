a987c0fd68110846379445c03fb7a1d02de49190
Samia-almtlek <samia.almoutlak@gmail.com>
2023-10-02 12:12:17 +0200

    Update e2_dynamisch.mjs

--- e2_dynamisch.mjs
+++ e2_dynamisch.mjs
@@ -8,6 +8,14 @@ const userInput = readline.createInterface({input, output});
 
 // Voeg hier je eigen code in
 
+let name = await userInput.question('Wat is je naam? ');
+console.log(name);
+
+let age = await userInput.question('Wat is je oud? ');
+console.log(age);
+
+let favoriteGame = await userInput.question('Wat is je favoriteGame? ');
+console.log(favoriteGame);
 
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
