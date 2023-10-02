4e22be92a62551f88a7e37152efc5cb68dd47034
Samia-almtlek <samia.almoutlak@gmail.com>
2023-10-02 12:04:10 +0200

    Update e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,6 @@
 // Voeg hier je eigen code in
-
-
+let name ='Samia';
+let age='25';
+let favoriteGame ='Pubg';
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
