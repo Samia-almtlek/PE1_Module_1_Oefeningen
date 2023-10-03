30dc785d0e0c1c8008948af21eb37ee6fd4c2cc7
Samia-almtlek <samia.almoutlak@gmail.com>
2023-10-03 16:00:33 +0200

    Create M2.mjs

---
+++ M2.mjs
@@ -0,0 +1,4 @@
+let geboorteJaar= 1997 ;
+let toekomstJaar= 2031 ;
+let jaar = toekomstJaar - geboorteJaar ;
+console.log(' In ' + toekomstJaar + ' zal ik ' + jaar +' of ' + ++jaar + ' jaar oud zijn.')
